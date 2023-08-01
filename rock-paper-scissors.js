cf26c28b0b5317d2d64312015808118c5d5b17dd
aaronj24680 <aaronjanusitis@gmail.com>
2023-08-01 16:26:21 -0400

    Added score keeping and game results on page

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,39 +10,83 @@ function gameRound(playerSelection, computerSelection) {
         return "It's a tie!";
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
+        botScore += 1;
         return "You lose! Paper beats rock";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore += 1;
         return "You win! Rock beats scissors";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
+        botScore += 1;
         return "You lose! Scissors beats paper";
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore += 1;
         return "You win! Paper beats rock";
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
+        botScore += 1;
         return "You lose! Rock beats scissors";
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore += 1;
         return "You win! Scissors beats paper";
     }
     else {
         return "No results, this shouldn't happen";
     }
 }
+function updateScores() {
+    playerScoreContainer.textContent = "Player: " + playerScore;
+    botScoreContainer.textContent = "Bot: " + botScore;
+}
+
+let gameRounds = 0;
+let playerScore = 0;
+let botScore = 0;
+
+const playerScoreContainer = document.querySelector('.player_score');
+const botScoreContainer = document.querySelector('.bot_score');
+const resultsContainer = document.querySelector('.results');
+const results = document.createElement('p');
+const endGameContainer = document.querySelector('.end_game');
+const endGame = document.createElement('p');
 
 const rockButton = document.querySelector('#rock_button');
 rockButton.addEventListener('click', () => {
-    console.log(gameRound("rock", getComputerChoice()));
+    
+    results.textContent = gameRound("rock", getComputerChoice());
+    resultsContainer.appendChild(results);
+
+    gameRounds += 1;
+    if(gameRounds == 5) {
+        endGame.textContent = "End of game!";
+        endGameContainer.appendChild(endGame);
+    }
+    updateScores();
   });
 
 const paperButton = document.querySelector('#paper_button');
 paperButton.addEventListener('click', () => {
-    console.log(gameRound("paper", getComputerChoice()));
+    results.textContent = gameRound("paper", getComputerChoice());
+    resultsContainer.appendChild(results);
+
+    gameRounds += 1;
+    if(gameRounds == 5) {
+        endGame.textContent = "End of game!";
+        endGameContainer.appendChild(endGame);
+    }
   });
 
 const scissorsButton = document.querySelector('#scissors_button');
 scissorsButton.addEventListener('click', () => {
-    console.log(gameRound("scissors", getComputerChoice()));
+    results.textContent = gameRound("scissors", getComputerChoice());
+    resultsContainer.appendChild(results);
+
+    gameRounds += 1;
+    if(gameRounds == 5) {
+        endGame.textContent = "End of game!";
+        endGameContainer.appendChild(endGame);
+    }
   });
