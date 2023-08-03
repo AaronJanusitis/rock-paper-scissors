6587ad8b268249cd894d90b7871baa5c01c8a5dd
aaronj24680 <aaronjanusitis@gmail.com>
2023-08-03 15:09:36 -0400

    Finished JavaScript part of project and added CSS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,31 +7,31 @@ function getComputerChoice() {
 //Declaring a function that will play a single round of the game. Takes the player's input and computer's input and returns the winner as a string
 function gameRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "It's a tie!";
+        return "This round is a tie!";
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
         botScore += 1;
-        return "You lose! Paper beats rock";
+        return "You lose this round! Paper beats rock";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore += 1;
-        return "You win! Rock beats scissors";
+        return "You win this round! Rock beats scissors";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         botScore += 1;
-        return "You lose! Scissors beats paper";
+        return "You lose this round! Scissors beats paper";
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore += 1;
-        return "You win! Paper beats rock";
+        return "You win this round! Paper beats rock";
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         botScore += 1;
-        return "You lose! Rock beats scissors";
+        return "You lose this round! Rock beats scissors";
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore += 1;
-        return "You win! Scissors beats paper";
+        return "You win this round! Scissors beats paper";
     }
     else {
         return "No results, this shouldn't happen";
@@ -41,6 +41,25 @@ function updateScores() {
     playerScoreContainer.textContent = "Player: " + playerScore;
     botScoreContainer.textContent = "Bot: " + botScore;
 }
+function afterRound() {
+    gameRounds += 1;
+    resultsTitle.textContent = "Results - Game " + gameRounds + "/5";
+    if(gameRounds == 5) {
+        document.querySelector("#rock_button").disabled = true;
+        document.querySelector("#paper_button").disabled = true;
+        document.querySelector("#scissors_button").disabled = true;
+
+        if (playerScore > botScore) {
+            endGame.textContent = "End of game! You won the match! Click Reset to play again.";
+        }
+        else if(botScore > playerScore) {
+            endGame.textContent = "End of game! You lost the match! Click Reset to play again.";
+        }
+        else {
+            endGame.textContent = "End of game! You tied the match! Click Reset to play again.";
+        }
+    }
+}
 
 let gameRounds = 0;
 let playerScore = 0;
@@ -48,45 +67,41 @@ let botScore = 0;
 
 const playerScoreContainer = document.querySelector('.player_score');
 const botScoreContainer = document.querySelector('.bot_score');
-const resultsContainer = document.querySelector('.results');
-const results = document.createElement('p');
-const endGameContainer = document.querySelector('.end_game');
-const endGame = document.createElement('p');
-
+const resultsTitle = document.querySelector('.results');
+const resultsText = document.querySelector('.results_text');
+const endGame = document.querySelector('.end_game');
 const rockButton = document.querySelector('#rock_button');
-rockButton.addEventListener('click', () => {
-    
-    results.textContent = gameRound("rock", getComputerChoice());
-    resultsContainer.appendChild(results);
+const paperButton = document.querySelector('#paper_button');
+const scissorsButton = document.querySelector('#scissors_button');
+const resetButton = document.querySelector('#reset_button');
 
-    gameRounds += 1;
-    if(gameRounds == 5) {
-        endGame.textContent = "End of game!";
-        endGameContainer.appendChild(endGame);
-    }
+resetButton.addEventListener('click', () => {
+    gameRounds = 0;
+    playerScore = 0;
+    botScore = 0;
+    updateScores();
+    document.querySelector("#rock_button").disabled = false;
+    document.querySelector("#paper_button").disabled = false;
+    document.querySelector("#scissors_button").disabled = false;
+    resultsText.textContent = "";
+    endGame.textContent = "";
+    resultsTitle.textContent = "Results - Game 0/5";
+  });
+
+rockButton.addEventListener('click', () => {
+    resultsText.textContent = gameRound("rock", getComputerChoice());
+    afterRound();
     updateScores();
   });
 
-const paperButton = document.querySelector('#paper_button');
 paperButton.addEventListener('click', () => {
-    results.textContent = gameRound("paper", getComputerChoice());
-    resultsContainer.appendChild(results);
-
-    gameRounds += 1;
-    if(gameRounds == 5) {
-        endGame.textContent = "End of game!";
-        endGameContainer.appendChild(endGame);
-    }
+    resultsText.textContent = gameRound("paper", getComputerChoice());
+    afterRound();
+    updateScores();
   });
 
-const scissorsButton = document.querySelector('#scissors_button');
 scissorsButton.addEventListener('click', () => {
-    results.textContent = gameRound("scissors", getComputerChoice());
-    resultsContainer.appendChild(results);
-
-    gameRounds += 1;
-    if(gameRounds == 5) {
-        endGame.textContent = "End of game!";
-        endGameContainer.appendChild(endGame);
-    }
+    resultsText.textContent = gameRound("scissors", getComputerChoice());
+    afterRound();
+    updateScores();
   });
